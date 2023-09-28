eaec26ebd8808fc5dc6ec68ec6071243be11e468
emiurruti <emiurruti98>
2023-09-28 11:04:36 -0300

    actualizacion

---
+++ agregación.py
@@ -0,0 +1,10 @@
+#agregación clase y autor
+
+class Libro:
+    def __init__(self, título="", género="", ISBN=""):
+        self.aventura = título
+        self.narrativo = género
+        self.ISBN = ISBN
+        None
+    
+
